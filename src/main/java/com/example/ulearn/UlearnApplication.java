b69c1a188c61443b90ed885f8fdb2f74c1ed7c84
danila <lysunckin.danil@yandex.ru>
2023-08-22 23:33:18 +0500

    new flask method version for hosting

--- src/main/java/com/example/ulearn/UlearnApplication.java
+++ src/main/java/com/example/ulearn/UlearnApplication.java
@@ -10,19 +10,37 @@ import java.io.IOException;
 public class UlearnApplication {
 
     public static void main(String[] args) {
-        flask();
+        flaskLocal();
         SpringApplication.run(UlearnApplication.class, args);
     }
 
-    static void flask() {
+    static void flaskServer() {
+        String dir = new File("").getAbsolutePath();
+        File python = new File(dir + "/BOOT-INF/classes/flaskUlearn/venv/bin/python");
+        File app = new File(dir + "/BOOT-INF/classes/flaskUlearn/app.py");
+
+        try {
+            Process process0 = new ProcessBuilder("jar", "xf", "ulearn-0.0.1-SNAPSHOT.jar", "/BOOT-INF/classes/flaskUlearn").start();
+            Thread.sleep(500);
+            Process process1 = new ProcessBuilder("chmod", "+x", python.getPath()).start();
+            Thread.sleep(500);
+            Process process2 = new ProcessBuilder(python.getPath(), app.getPath()).start();
+            Process process3 = new ProcessBuilder("ngrok", "http", "5000").start();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static void flaskLocal() {
         File python = new File("src/main/resources/flaskUlearn/venv/bin/python");
         File app = new File("src/main/resources/flaskUlearn/app.py");
         try {
-            Process process = new ProcessBuilder(python.getAbsolutePath(), app.getAbsolutePath()).start();
-            Process process2 = new ProcessBuilder("ngrok", "http", "5000").start();
+            Process process0 = new ProcessBuilder(python.getAbsolutePath(), app.getAbsolutePath()).start();
+            Process process1 = new ProcessBuilder("ngrok", "http", "5000").start();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
 
 }
