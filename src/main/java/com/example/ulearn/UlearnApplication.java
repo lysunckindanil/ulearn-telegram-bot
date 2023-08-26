6e6fe8cdbe93f0e7df1f0f672f5e2b58e555e2d7
danila <lysunckin.danil@yandex.ru>
2023-08-26 15:25:20 +0500

    delete flask and ngrok execution processes

--- src/main/java/com/example/ulearn/UlearnApplication.java
+++ src/main/java/com/example/ulearn/UlearnApplication.java
@@ -10,19 +10,7 @@ import java.io.IOException;
 public class UlearnApplication {
 
     public static void main(String[] args) {
-        flask();
         SpringApplication.run(UlearnApplication.class, args);
-    }
-
-    static void flask() {
-        File python = new File("src/main/resources/flaskUlearn/venv/bin/python");
-        File app = new File("src/main/resources/flaskUlearn/app.py");
-        try {
-            Process process = new ProcessBuilder(python.getAbsolutePath(), app.getAbsolutePath()).start();
-            Process process2 = new ProcessBuilder("ngrok", "http", "5000").start();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 
 }
